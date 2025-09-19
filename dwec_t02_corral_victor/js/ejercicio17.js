d8554cfa20206c69520913695b6029bf64779352
Victor M. Corral <victor.manuel.corral.ruiz@gmail.com>
2025-09-19 13:30:53 +0200

    Agregando comprobacion multiple

--- dwec_t02_corral_victor/js/ejercicio17.js
+++ dwec_t02_corral_victor/js/ejercicio17.js
@@ -6,35 +6,38 @@ Un número es perfecto si la suma de sus divisores propios (excluyendo el númer
 Debes validar que el número introducido sea un entero positivo y mayor que 0.
 */
 
-let num = 0;
-let arrayDivisores = []
-let suma = 0;
-let index = 0;
 
 do {
-    num = Number(prompt("Introduce el numero a comprobar: "));
-} while (num <=0 || !Number.isInteger(num));
 
-
-for (let i = 0; i < num; i++){
-    if (num%i == 0){
-        arrayDivisores[index] = i;
-        index ++;
-        console.log(index);
+    let num = 0;
+    let arrayDivisores = []
+    let suma = 0;
+    let index = 0;
+    
+    do {
+        num = Number(prompt("Introduce el numero a comprobar: "));
+    } while (num <=0 || !Number.isInteger(num));
+    
+    
+    for (let i = 0; i < num; i++){
+        if (num%i == 0){
+            arrayDivisores[index] = i;
+            index ++;
+        }
+    };
+    
+    
+    for (let i = 0; i< arrayDivisores.length; i++){
+        suma += arrayDivisores[i];
     }
-};
+    
+    
+    if (suma == num){
+        alert("El numero " + num + " es un numero perfecto");
+    } else {
+        alert("El numero " + num + " no es un numero perfecto");
+    }
+    
+    console.log(arrayDivisores)
 
-
-for (let i = 0; i< arrayDivisores.length; i++){
-    suma += arrayDivisores[i];
-}
-
-
-if (suma == num){
-    alert("El numero " + num + " es un numero perfecto");
-} else {
-    alert("El numero " + num + " no es un numero perfecto");
-}
-
-
-console.log(arrayDivisores)
+} while (confirm("¿Quieres comprobar otro numero?"))
