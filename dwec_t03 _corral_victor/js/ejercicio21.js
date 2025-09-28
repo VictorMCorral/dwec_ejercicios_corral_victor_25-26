1478fbb70d5b62f0536ec50a6012c69a7ff8c7dc
VictorMCorral <victor.manuel.corral.ruiz@gmail.com>
2025-09-28 12:50:19 +0200

    Agregando ejercicios 21 y 22

--- dwec_t03 _corral_victor/js/ejercicio21.js
+++ dwec_t03 _corral_victor/js/ejercicio21.js
@@ -11,16 +11,29 @@ La expresión regular debes crear usando el método:
     -- var patt = /pattern/modifiers;
 
 Puedes usar una IA para generar el patrón, entendiendo dicho patrón.
+
+Hecho esto, definirás una función que se llame validarPrefijoTeléfonoEsp() 
+que permita validar si un teléfono introducido tiene el prefijo +34. 
+Y para terminar harás una función llamada validarTelefonoConSin() que invoque a las 
+dos funciones anteriores, según sea conveniente.
 */
 
 let numeroTelefono = prompt("Introduce un numero de telefono: ");
 
-if (validaTelefono(numeroTelefono)){
+if (validarTelefonoConSin(numeroTelefono)){
     console.log("Es un telefono movil o fijo valido")
 } else {
     console.log("No es un telefono valido")
 }
 
+
+function validarTelefonoConSin(telefono) {
+    if (telefono.length == 9 ){
+        return validaTelefono(telefono);
+    } else if (telefono.length == 12){
+        return validarPrefijoTeléfonoEsp(telefono);
+    }
+}
 
 
 function validaTelefono(telefono){
@@ -35,3 +48,13 @@ function validaTelefono(telefono){
     }
 
 };
+
+function validarPrefijoTeléfonoEsp(telefono){
+    if (telefono.substring(0,3) == "+34"){
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
---
+++ dwec_t03 _corral_victor/js/ejercicio22.js
@@ -0,0 +1,105 @@
+console.log("T03 - Ejercicio 22");
+/*
+Busca en internet dos expresiones regulares. Una que permita validar un DNI y otra que permita validar un CIF. 
+Crea script que pida al usuario una cadena y determine si es DNI o CIF válido. 
+Es necesario que definas una función que se denomine "validarDNIyCIF()" que reciba una cadena y devuelva un booleano.
+
+La expresión regular debes crear usando el método: 
+
+var patt =/pattern/modifiers;
+
+Después, busca el algoritmo que permite comprobar si el DNI o el CIF 
+son correctos (es decir, si corresponde la letra al número)
+
+Puedes usar una IA para generar el patrón y el segundo algoritmo de comprobación, entiendo el código dado.
+*/
+
+
+// dato % 23 = resto -->  letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+let dato = prompt("Introduce tu dni o cif valido: ");
+
+
+if (validarDNIyCIF(dato)) {
+    console.log("El dato introducido es un DNI o CIF válido.");
+} else {
+    console.log("El dato introducido no es DNI o CIF válido.");
+}
+
+function validarDNIyCIF(dato) {
+    const dniRegex = /^[0-9]{8}[A-Z]$/i;
+    const cifRegex = /^[ABCDEFGHJKLMNPQRSUVW][0-9]{7}[0-9A-J]$/i;
+
+    if (dniRegex.test(dato)) {
+        return validarDNI(dato);
+    } else if (cifRegex.test(dato)) {
+        return validarCIF(dato);
+    } else {
+        return false;
+    }
+
+}
+
+function validarDNI(dato) {
+    let letras = "TRWAGMYFPDXBNJZSQVHLCKE"
+    let letra = dato.substring(dato.length - 1).toUpperCase();
+    let numero = dato.substring(0, dato.length - 1);
+
+
+    if (numero % 23 == letras.indexOf(letra)) {
+        console.log("El DNI es correcto");
+        return true;
+    } else {
+        console.log("El dni no es correcto");
+        return false;
+    }
+
+
+}
+
+function validarCIF(dato) {
+    const cifRegexNum = /^[ABCDEFGHJKLMNPQRSUVW][0-9]{7}[0-9]$/i;  // CIF que termina en número
+    const cifRegexLetra = /^[ABCDEFGHJKLMNPQRSUVW][0-9]{7}[A-J]$/i; // CIF que termina en letra
+
+    let principio = dato.substring(0, 1).toUpperCase();
+    let medio = dato.substring(1, 8);
+    let final = dato.substring(8, 9).toUpperCase();
+
+    let digitosImpares = 0;
+    let digitosPares = 0;
+
+    for (let i = 0; i < medio.length; i++) {
+        let digito = parseInt(medio.substring(i, i + 1));
+
+        if (i % 2 === 0) { // posiciones impares
+            let temp = digito * 2;
+            digitosImpares += Math.floor(temp / 10) + (temp % 10);
+        } else { // posiciones pares
+            digitosPares += digito;
+        }
+    }
+
+    let sumaTotalMedio = digitosImpares + digitosPares;
+    let control = (10 - (sumaTotalMedio % 10)) % 10;
+
+    // CIF que termina en letra
+    if (cifRegexLetra.test(dato)) {
+        if ("KPQS".includes(principio)) {
+            let controlLetras = "JABCDEFGHI";
+            return final === controlLetras[control];
+        } else {
+            return false;
+        }
+    }
+    // CIF que termina en número
+    else if (cifRegexNum.test(dato)) {
+        if ("ABEH".includes(principio)) {
+            return final === String(control);
+        } else {
+            return false;
+        }
+    } else {
+        console.log("No es un CIF válido");
+        return false;
+    }
+}
+
